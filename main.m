9681e286bf8e9dfb212576af2a3da1534b3db7d6
takuumi <satu_noone@hotmail.com>
2020-07-25 00:07:02 +0900

    lqr仮実装 ファイル名変更

--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 clear
 format compact
 
+state=[0;0];
+bPID = true;
+
 g = 9.8; %9.80665 [m/s^2]
 L = 0.3; %[m] 
 m = 0.2; %[kg]
@@ -10,12 +13,15 @@ B = [0; 1/m];
 C = [1, 0];
 D = 0;
 
-Q = [1,0;
-     0,0];
+Q = [2,0;
+     0,0.1];
 R = 1;
 
 [K,S,P] = lqr(A,B,Q,R);
-%sys1 = ss(A-B*K,B,C,D);
-%step(sys1)
 
-%open_system('simulate');
+open_system('simulate_pid');
+sim('simulate_pid');
+
+open_system('simulate_lqr');
+sim('simulate_lqr');
+
