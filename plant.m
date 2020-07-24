5d9e64f633bdebdc54e1e88e02bbeb1c605f268d
takuumi <satu_noone@hotmail.com>
2020-07-24 10:29:01 +0900

    PID コントローラ

--- plant.m
+++ plant.m
@@ -1,9 +1,21 @@
-g = 9.8 %9.80665 [m/s^2]
-L = 0.3 %[m] 
-m = 0.2 %[kg]
+clear
+format compact
+
+g = 9.8; %9.80665 [m/s^2]
+L = 0.3; %[m] 
+m = 0.2; %[kg]
 
 A = [0, 1; -g/L, 0];
 B = [0; 1/m];
 C = [1, 0];
+D = 0;
 
-open_system('simulate');
+Q = [1,0;
+     0,0];
+R = 1;
+
+[K,S,P] = lqr(A,B,Q,R);
+%sys1 = ss(A-B*K,B,C,D);
+%step(sys1)
+
+%open_system('simulate');
